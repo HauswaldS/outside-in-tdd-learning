c5a9fdb40005c01942ab8cddfa6f071d9fd3ed12
Axel Le Borgne <lbaxel95@gmail.com>
2021-11-04 10:23:23 +0100

    RestaurantForm integration

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,4 +1,5 @@
 import RestaurantList from './RestaurantList';
+import NewRestaurantForm from './NewRestaurantForm';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -7,6 +8,7 @@ const RestaurantScreen = () => (
   <Card>
     <CardContent>
       <Typography variant="h5">Restaurants</Typography>
+      <NewRestaurantForm />
       <RestaurantList />
     </CardContent>
   </Card>
